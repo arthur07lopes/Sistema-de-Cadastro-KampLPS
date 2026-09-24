c6a20d6a21dd9e71cbe4b02930b00b8d2cebb239
Arthur Santos Lopes <arthur.s.lopes7@aluno.senai.br>
2026-09-24 13:49:19 -0300

    Initialize wxPython application with main window

---
+++ main.py
@@ -0,0 +1,11 @@
+import wx
+
+from logica.gerenciador_pessoas import GerenciadorPessoas
+from ui.janela_principal import JanelaPrincipal
+
+if __name__ == "__main__":
+  app = wx.App()
+  gerenciador = GerenciadorPessoas()
+  janela = JanelaPrincipal()
+  janela.show()
+  app.MainLoop()
